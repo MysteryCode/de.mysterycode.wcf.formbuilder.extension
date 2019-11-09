dfa1ba22a8ace5c74b2d96581e55ec88dc3393dd
mutec <mysterycode@mysterycode.de>
2019-11-09 21:16:56 +0100

    generic attachment form field/container

---
+++ files/lib/system/form/builder/container/AttachmentFormContainer.class.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace wcf\system\form\builder\container;
+
+use wcf\data\IStorableObject;
+use wcf\data\object\type\ObjectTypeCache;
+use wcf\system\attachment\AttachmentHandler;
+use wcf\system\event\EventHandler;
+use wcf\system\form\builder\field\AttachmentFormField;
+
+class AttachmentFormContainer extends FormContainer {
+	/**
+	 * attachment form field
+	 * @var	AttachmentFormField
+	 */
+	protected $attachmentField;
+	
+	/**
+	 * attachment-related data used to create an `AttachmentHandler` object for the attachment
+	 * form field
+	 * @var	null|array
+	 */
+	protected $attachmentData;
+	
+	/**
+	 * id of the edited object
+	 * @var	integer
+	 */
+	protected $objectId;
+	
+	/**
+	 * id of the field itself
+	 * @var	integer
+	 */
+	protected $fieldId;
+	
+	/**
+	 * Sets the attachment-related data used to create an `AttachmentHandler` object for the
+	 * attachment form field. If no attachment data is set, attachments are not supported.
+	 *
+	 * By default, no attachment data is set.
+	 *
+	 * @param	null|string	$objectType		name of attachment object type or `null` to unset previous attachment data
+	 * @param	integer		$parentObjectID		id of the parent of the object the attachments belong to or `0` if no such parent exists
+	 * @return	static					this form container
+	 * @throws	\BadMethodCallException			if the attachment form field has already been initialized
+	 */
+	public function attachmentData($objectType = null, $parentObjectID = 0) {
+		if ($this->attachmentField !== null) {
+			throw new \BadMethodCallException("The attachment form field has already been initialized. Use the atatchment form field directly to manipulate attachment data.");
+		}
+		
+		if ($objectType === null) {
+			$this->attachmentData = null;
+		}
+		else {
+			if (ObjectTypeCache::getInstance()->getObjectTypeByName('com.woltlab.wcf.attachment.objectType', $objectType) === null) {
+				throw new \InvalidArgumentException("Unknown attachment object type '{$objectType}'.");
+			}
+			
+			$this->attachmentData = [
+				'objectType' => $objectType,
+				'parentObjectID' => $parentObjectID
+			];
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Returns the form field handling attachments.
+	 *
+	 * @return	AttachmentFormField
+	 * @throws	\BadMethodCallException		if the form field container has not been populated yet/form has not been built yet
+	 */
+	public function getAttachmentField() {
+		if ($this->attachmentField === null) {
+			throw new \BadMethodCallException("attachment form field can only be requested after the form has been built.");
+		}
+		
+		return $this->attachmentField;
+	}
+	
+	/**
+	 * Returns the id of the edited object or `0` if no object is edited.
+	 *
+	 * @return	integer
+	 */
+	public function getObjectId() {
+		return $this->objectId;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function id($id) {
+		$this->fieldId = $id;
+		
+		return parent::id($id . 'Container');
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function loadValues(array $data, IStorableObject $object) {
+		$this->objectId = $object->{$object::getDatabaseTableIndexName()};
+		
+		if ($this->attachmentData !== null) {
+			// updated attachment handler with object id
+			$this->attachmentField->attachmentHandler(
+				new AttachmentHandler(
+					$this->attachmentData['objectType'],
+					$this->getObjectId(),
+					'.',
+					$this->attachmentData['parentObjectID']
+				)
+			);
+		}
+		
+		return parent::loadValues($data, $object);
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function populate() {
+		parent::populate();
+		
+		$this->attachmentField = AttachmentFormField::create($this->fieldId);
+		
+		$this->appendChildren([
+			$this->attachmentField
+		]);
+		
+		if ($this->attachmentData !== null) {
+			$this->attachmentField->attachmentHandler(
+			// the temporary hash may not be empty (at the same time as the
+			// object id) and it will be changed anyway by the called method
+				new AttachmentHandler(
+					$this->attachmentData['objectType'],
+					$this->getObjectId(),
+					'.',
+					$this->attachmentData['parentObjectID']
+				)
+			);
+		}
+		
+		EventHandler::getInstance()->fireAction($this, 'populate');
+	}
+}
---
+++ files/lib/system/form/builder/field/AttachmentFormField.class.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace wcf\system\form\builder\field;
+
+use wcf\system\attachment\AttachmentHandler;
+use wcf\system\form\builder\data\processor\CustomFormDataProcessor;
+use wcf\system\form\builder\IFormDocument;
+use wcf\system\WCF;
+use wcf\util\StringUtil;
+
+class AttachmentFormField extends AbstractFormField {
+	/**
+	 * attachment handler
+	 * @var	null|AttachmentHandler
+	 */
+	protected $attachmentHandler;
+	
+	/**
+	 * @inheritDoc
+	 */
+	protected $javaScriptDataHandlerModule = 'WoltLabSuite/Core/Form/Builder/Field/Wysiwyg/Attachment';
+	
+	/**
+	 * @inheritDoc
+	 */
+	protected $templateName = '__wysiwygAttachmentFormField';
+	
+	/**
+	 * Creates a new instance of `AttachmentFormField`.
+	 */
+	public function __construct() {
+		$this->addClass('wide');
+	}
+	
+	/**
+	 * Sets the attachment handler object for the uploaded attachments. If `null` is given,
+	 * the previously set attachment handler is unset.
+	 *
+	 * For the initial attachment handler set by this method, the temporary hashes will be
+	 * automatically set by either reading them from the session variables if the form handles
+	 * AJAX requests or by creating a new one. If the temporary hashes are read from session,
+	 * the session variable will be unregistered afterwards.
+	 *
+	 * @param	null|AttachmentHandler		$attachmentHandler
+	 * @return	static
+	 */
+	public function attachmentHandler(AttachmentHandler $attachmentHandler = null) {
+		if ($attachmentHandler !== null) {
+			if ($this->attachmentHandler === null) {
+				$tmpHash = StringUtil::getRandomID();
+				if ($this->getDocument()->isAjax()) {
+					$sessionTmpHash = WCF::getSession()->getVar('__wcfAttachmentTmpHash');
+					if ($sessionTmpHash !== null) {
+						$tmpHash = $sessionTmpHash;
+						
+						WCF::getSession()->unregister('__wcfAttachmentTmpHash');
+					}
+				}
+				
+				$attachmentHandler->setTmpHashes([$tmpHash]);
+			}
+			else {
+				// preserve temporary hashes
+				$attachmentHandler->setTmpHashes($this->attachmentHandler->getTmpHashes());
+			}
+		}
+		
+		$this->attachmentHandler = $attachmentHandler;
+		
+		if ($this->attachmentHandler !== null) {
+			$this->description('wcf.attachment.upload.limits', [
+				'attachmentHandler' => $this->attachmentHandler
+			]);
+		}
+		else {
+			$this->description();
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Returns the attachment handler object for the uploaded attachments or `null` if no attachment
+	 * upload is supported.
+	 *
+	 * @return	null|AttachmentHandler
+	 */
+	public function getAttachmentHandler() {
+		return $this->attachmentHandler;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function hasSaveValue() {
+		return false;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function isAvailable() {
+		return parent::isAvailable() && $this->getAttachmentHandler() !== null;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function populate() {
+		parent::populate();
+		
+		$this->getDocument()->getDataHandler()->addProcessor(new CustomFormDataProcessor($this->getId(), function(IFormDocument $document, array $parameters) {
+			if ($this->getAttachmentHandler() !== null) {
+				$parameters[$this->getId() . '_attachmentHandler'] = $this->getAttachmentHandler();
+			}
+			
+			return $parameters;
+		}));
+		
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function readValue() {
+		if ($this->getDocument()->hasRequestData($this->getPrefixedId() . '_tmpHash')) {
+			$tmpHash = $this->getDocument()->getRequestData($this->getPrefixedId() . '_tmpHash');
+			if (is_string($tmpHash)) {
+				$this->getAttachmentHandler()->setTmpHashes([$tmpHash]);
+			}
+			else if (is_array($tmpHash)) {
+				$this->getAttachmentHandler()->setTmpHashes($tmpHash);
+			}
+		}
+	}
+}
