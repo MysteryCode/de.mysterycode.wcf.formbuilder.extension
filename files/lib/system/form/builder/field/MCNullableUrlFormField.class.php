df426cd44567db045a7425382718db4b1a9a26cb
mutec <mysterycode@mysterycode.de>
2022-06-19 13:16:14 +0200

    add nullable url form field

---
+++ files/lib/system/form/builder/field/MCNullableUrlFormField.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace wcf\system\form\builder\field;
+
+class MCNullableUrlFormField extends UrlFormField implements INullableFormField {
+	use TNullableFormField;
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function getSaveValue() {
+		if ($this->getValue() === null && !$this->isNullable()) {
+			return '';
+		}
+		
+		return parent::getSaveValue();
+	}
+}
